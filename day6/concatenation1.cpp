e8369f04fd76d11867c74fc71e8e5a68508eabaa
Surajsharma0804 <surajkumarind08@gmail.com>
2025-06-20 12:30:22 +0530

    concatenation

---
+++ day6/concatenation1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string number = "123";
+    string text = "Hii";
+    string concatenated = number + text;
+    cout << "Concatenated result: " << concatenated; // This will concatenate the string representation of the number and the text
+    return 0;
+}
