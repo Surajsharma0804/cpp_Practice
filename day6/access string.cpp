af02ffd979835d6e1a70d809b0e5c3f6bb4b91e1
Surajsharma0804 <surajkumarind08@gmail.com>
2025-06-20 12:45:04 +0530

    String

---
+++ day6/access string.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string access = "Access";
+    cout << "The first character of the string is : " << access[0] << endl; // Accessing the first character of the string
+    cout << "The second character of the string is: " << access[1] << endl;
+    cout << "The 4th character of the string is: " << access[3] << endl; // Accessing the 4th character of the string
+    return 0;
+}
