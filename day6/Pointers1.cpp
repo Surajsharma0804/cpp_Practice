f3d8f7e1dac849c445ec025a5c3325450f5e1039
Surajsharma0804 <surajkumarind08@gmail.com>
2025-06-20 19:01:33 +0530

    Pointer1

---
+++ day6/Pointers1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    int b = 15;
+    // int *ptra = &a; // Pointer to a
+    // int *ptrb = &b; // Pointer to b
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
+    cout << "The address of a is: " << &a << endl;
+    cout << "The address of b is: " << &b << endl;
+    return 0;
+}
