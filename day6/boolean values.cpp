ade1c2c583d2b074bb27114627d1f6592bf40ff3
Surajsharma0804 <surajkumarind08@gmail.com>
2025-06-20 13:29:06 +0530

    Boolean

---
+++ day6/boolean values.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    bool isTrue = true; // Initializing a boolean variable with true
+    bool isFalse = false; // Initializing a boolean variable with false
+    cout << "The value of isTrue is: " << isTrue <<endl;
+    cout << "The value of isFalse is: " << isFalse ;
+    return 0;
+}
