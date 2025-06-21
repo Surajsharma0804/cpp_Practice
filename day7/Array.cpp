1b168c697cba08438528979d3ef64a9098de8690
Surajsharma0804 <surajkumarind08@gmail.com>
2025-06-21 11:32:15 +0530

    Array

---
+++ day7/Array.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string cars[5] = {"BMW", "Audi", "Mercedes", "Toyota", "Lamborghini"};
+    cout << cars[4] << endl; // Accessing the 5th element of the array
+    cout <<cars[0] <<endl; // Accessing the 1st element of the array
+    return 0;
+}
